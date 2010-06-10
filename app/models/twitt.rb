a30cca4c7a81bdf9e62f302cb214c98fbc53acfc
Ania\ Leśniak <anna.lesniak@llp.pl>
2010-06-10 14:41:34 +0200

    Init app

---
+++ app/models/twitt.rb
@@ -0,0 +1,11 @@
+class Twitt
+
+  include Mongoid::Document
+
+  field :twitt_id
+  field :parent_id, :index => true
+  field :date
+  field :body
+  field :user_id
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '7e41a7c4077d391c4d16f39660b350f08829f0ab9ba4fb9b46f9fa03e80cb4e6fdb80703ea797c195e77a488a3e456974ea99f3739b1f9eaf91f01e7ff17d854'
